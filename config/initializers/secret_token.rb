39a31b7a7b755b38c959a29c7e82eb8b8bc78aef
Purnam Jantrania <pjantrania@gmail.com>
2012-05-27 16:28:37 -0400

    Created base rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheCourseForum::Application.config.secret_token = '57becfb4c1b1a34a860cdc9ef66f557a89cbc66491bd05695f8dd721d685da796b9ac8a023344013bd62d002f63a4347be267433340ed8e4e4a1b142ad941b84'
