7ded2d05752227d7534a95a23bc0c34ce4e067fb
Purnam Jantrania <pjantrania@gmail.com>
2013-02-02 11:46:27 -0500

    added to users model & registration page

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,7 @@ class User < ActiveRecord::Base
   before_save { |user| user.email = email.downcase }
 
   validates :name, :presence => true, :length => { :maximum => 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\.\-]+@([\w+\.]+\w\.)?virginia.edu\z/i
   validates :email, :presence => true, :format => { :with => VALID_EMAIL_REGEX },
   :uniqueness => { :case_sensitive => false }
   validates :password, :presence => true,
