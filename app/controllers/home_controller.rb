9b046a31152df757a0a56076452a5461574c76b8
jeffthink <jeff.bordogna@gmail.com>
2013-03-10 16:31:09 -0400

    Sets up basic template and styling to be used across the site
    Highlights:
    
    * Introduces twitter bootstrap for scaffolding and consistent styling using the
    bootstrap-sass gem
    * Uses sass as a css preprocessor to give better ability to set css
    variables, mixins, nesting, etc. - all style classes should have
    file extension of .css.scss
    * Uses haml instead of erb to do template rendering because of its
    terseness...both can live in the same app, but you'll want to just one or the
    other going forward for consistency's sake
    * Creates a application layout that gives the template for all pages
    that users will see (who are logged in)
    * Creates a welcome layout for the template used on non-logged in users
    pages
    * Introduces simple_form gem for creating forms - this will become more
    valuable as the forms become more complex
    * The welcome page has a banner with login form, a area for marketing,
    and a signup form
    * The logged in user home page has a welcome banner, and a list of
    departments/schools, the latter is shared with the departments list page.
    
    Todo:
    
    * Set up a user system with devise or warden, and in the
    config/routes.rb file, uncomment the filter that sends a user to
    either welcome or home controller depending on logged in user status
    * Hook in the forms to actually do stuff
    * The nav links on the left do not update based on what page you're on
    * You'll probably want to introduce a flash area in your application
    layout to use for showing notifications to users on a page
    * Finish filling in the rest of the pages obviously
    
    Good luck and feel free to ask questions!

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,13 @@
 class HomeController < ApplicationController
+  #todo not sure how best to share this code between here and department controller
   def index
-  end
-  def browse
-  	@schools = School.all
-  	@departments = Department.all
+  	departments = Department.find(:all, :order => "name")
+    schools = School.find(:all, :order => "name")
+    artSchoolId = 1
+    engrSchoolId = 2
+
+    @artDeps = columnize(departments.select{|d| d.school_id == artSchoolId })
+    @engrDeps = columnize(departments.select{|d| d.school_id == engrSchoolId })
+    @otherSchools = columnize(schools.select{|s| s.id != artSchoolId && s.id != engrSchoolId })
   end
 end
