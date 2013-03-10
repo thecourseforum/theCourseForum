1bc554f2e1c3adbfdfd05c0711f834d80a8e66be
Justin Dao <tjd5at@virginia.edu>
2013-03-10 13:50:30 -0400

    Some views and stuff

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
+  	@user = User.new
+  	@student = Student.new
   end
   def browse
   	@schools = School.all
--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,6 @@ class Student < ActiveRecord::Base
   attr_accessible :first_name, :grad_year, :last_name
   has_many :majors, :through => :student_majors
   has_many :reviews
+  validates :first_name, presence: true
+  validates :grad_year, presence: true
 end
