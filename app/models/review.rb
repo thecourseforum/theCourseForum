e0c48a7a389cc1a5a786310edd36b6fbb1013a38
Justin Dao <tjd5at@virginia.edu>
2013-03-12 21:40:23 -0400

    Created Review Form. TextArea width won't change, TA Name not implemented, still needs functionality, added some CSS for the form. Also changed header bar color to blue becuase I think it looks better, but that can be changed back easily. Let me know what you think of the blue though.

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,6 @@ class Review < ActiveRecord::Base
   belongs_to :student
   belongs_to :semester
   belongs_to :course_professor
-  attr_accessible :comment
+  attr_accessible :comment, :professor_rating, :enjoyability, :difficulty, :amount_reading, 
+  	:amount_writing, :amount_group, :amount_homework, :only_tests, :recommend, :ta_name
 end
