150c8d129ba8f8684858374eccceb6db7354afcb
Justin Dao <tjd5at@virginia.edu>
2013-10-14 16:40:05 -0400

    Adds method to ignore subdepartments with no professors (and therefore no valid courses)

--- app/models/subdepartment.rb
+++ app/models/subdepartment.rb
@@ -1,4 +1,13 @@
 class Subdepartment < ActiveRecord::Base
   has_and_belongs_to_many :department
   has_many :courses
+
+  def professors_list
+    profs = []
+    self.courses.each do |course|
+      profs += course.professors_list
+    end
+
+    return profs
+  end
 end
