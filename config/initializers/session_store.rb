9a9594b588639121b3057557b6543336989778de
Justin Dao <tjd5at@virginia.edu>
2013-10-23 23:28:43 -0400

    Adds rememberable and fixes cookie duration

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-TheCourseForum::Application.config.session_store :cookie_store, key: '_theCourseForum_session', expire_after: 504.hours
+TheCourseForum::Application.config.session_store :cookie_store, key: '_theCourseForum_session'
