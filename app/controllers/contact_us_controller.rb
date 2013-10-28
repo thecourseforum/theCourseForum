648ddbe0ab5671aecec134c60722347e3791f932
Justin Dao <tjd5at@virginia.edu>
2013-10-28 13:32:59 -0400

    Updates anonymous feedback

--- app/controllers/contact_us_controller.rb
+++ app/controllers/contact_us_controller.rb
@@ -5,8 +5,9 @@ class ContactUsController < ApplicationController
   end
 
   def submit
+    binding.pry
     if params[:report][:type] == "problem"
-      if params[:report][:anonymous] == ""
+      if params[:report][:anonymous] == "1"
         ContactUsMailer.error_report(nil, params[:report][:url], params[:report][:description]).deliver
       else
         ContactUsMailer.error_report(params[:user_id], params[:report][:url], params[:report][:description]).deliver
@@ -16,7 +17,7 @@ class ContactUsController < ApplicationController
       end
     
     elsif params[:report][:type] == "feedback"
-      if params[:report][:anonymous] == ""
+      if params[:report][:anonymous] == "1"
         ContactUsMailer.feedback(nil, params[:report][:description]).deliver
       else
         ContactUsMailer.feedback(params[:user_id], params[:report][:description]).deliver
