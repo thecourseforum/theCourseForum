f0dc2f5d69d17d1191b6b40fbe6388317a411876
Justin Dao <tjd5at@virginia.edu>
2013-10-30 23:23:11 -0400

    Updates mailer to send to support@tcf instead of my email

--- app/mailers/contact_us_mailer.rb
+++ app/mailers/contact_us_mailer.rb
@@ -6,7 +6,7 @@ class ContactUsMailer < ActionMailer::Base
 
   def error_report(user_id, url, description)
     @user = (user_id != nil ? User.find(user_id) : nil)
-    @email = "tjd5at@virginia.edu"
+    @email = "support@thecourseforum.com"
     @url = url
     @description = description
     mail(to: @email, from: "error_reports@thecourseforum.com", subject: 'Error Report from ' + (@user != nil ? @user.email : "Anonymous"))
@@ -14,14 +14,14 @@ class ContactUsMailer < ActionMailer::Base
 
   def feedback(user_id, description)
     @user = (user_id != nil ? User.find(user_id) : nil)
-    @email = "tjd5at@virginia.edu"
+    @email = "support@thecourseforum.com"
     @description = description
     mail(to: @email, from: "feedback@thecourseforum.com", subject: 'Feedback from ' + (@user != nil ? @user.email : "Anonymous"))
   end
 
   def other(user_id, description)
     @user = User.find(user_id)
-    @email = "tjd5at@virginia.edu"
+    @email = "support@thecourseforum.com"
     @description = description
     mail(to: @email, from: "other@thecourseforum.com", subject: 'Other questions from ' + @user.email)
   end
