34c3e17afe714bacc63b5462c5e58450ab67b6d7
Justin Dao <tjd5at@virginia.edu>
2013-12-14 15:38:26 -0500

    Updates confirmation mailer for development and CSS for login

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,4 @@
 class HomeController < ApplicationController
-  skip_before_filter :authenticate_user!, :only => [ :about, :terms, :privacy]
-  skip_before_filter :check_info, :only => [ :about, :terms, :privacy]  
+  skip_before_action :authenticate_user!, :only => [ :about, :terms, :privacy]
+  skip_before_action :check_info, :only => [ :about, :terms, :privacy]  
 end
