ea5a7cbc393f69f92a28488303748488adc42312
Justin Dao <tjd5at@virginia.edu>
2013-12-14 19:28:10 -0500

    Updates new review dropdowns

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -70,54 +70,7 @@ $(document).ready(function() {
 	}
 	});
 
-	$("#subdept_select").bind("change", function(){
-		$("#professors").fadeOut("fast");
-		$("#courses").fadeOut("fast");
-		$("#course_select").empty();
-		var value = $(this).find(":selected").val();
-		$.ajax({
-			url: '/subdepartments/' + value,
-			dataType: 'json',
-			type: 'GET',
-			success: function(data) {
-				$("#courses").fadeIn("fast");
-				$.each(data, function(){
-					$('#course_select').append($("<option/>", {
-						value: this.id,
-						text: this.subdepartment.mnemonic + " " + this.course_number
-					}));
-				});			
-				
-				course_select.selectedIndex = -1;			
-				
-			}
-		});
-
-	});
-
-	$("#course_select").bind("change", function(){
-		$("#prof_select").empty();
-		var value = $(this).find(":selected").val();
-		$.ajax({
-			url: '/courses/' + value,
-			dataType: 'json',
-			type: 'GET',
-			success: function(data) {
-				$("#professors").fadeIn("fast");
-				$.each(data.professors_list, function(){
-					$('#prof_select').append($("<option/>", {
-						value: this.id,
-						text: this.last_name + ", " + this.first_name
-					}));
-				});			
-				var prof_select = document.getElementById("prof_select");
-				if(prof_select.length > 1){
-					prof_select.selectedIndex = -1;
-				}
-			}
-		});
-		
-	});
+	
 
 });
 
