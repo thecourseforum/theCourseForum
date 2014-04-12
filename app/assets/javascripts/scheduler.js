461442d4f47e450e356cdb6a47079496fc70dd40
Alan Wei <aw3as@virginia.edu>
2014-04-11 20:16:38 -0400

    skeleton scheduler calender shows glhf

--- app/assets/javascripts/scheduler.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-
-    // page is now ready, initialize the calendar...
-
-    $('#calendar').fullCalendar({
-        // put your options and callbacks here
-    })
-
-});
