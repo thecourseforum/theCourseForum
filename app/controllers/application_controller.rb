ce9257cfacabd304c2c116994bae72b976c492a3
Justin Dao <tjd5at@virginia.edu>
2014-04-15 12:51:09 -0400

    En-enables authenticate user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  #before_action :authenticate_user!
+  before_action :authenticate_user!
 
   before_action :check_info
 
