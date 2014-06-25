29ce9f8e787f9a57a20bc15cc516d35f9009810f
Justin Dao <tjd5at@virginia.edu>
2014-06-24 22:14:20 -0400

    Adds correct semester_id update for sections when migrating to new schema

--- db/migrate/20140413070856_add_semester_id_to_sections.rb
+++ db/migrate/20140413070856_add_semester_id_to_sections.rb
@@ -1,5 +1,12 @@
 class AddSemesterIdToSections < ActiveRecord::Migration
   def change
     add_column :sections, :semester_id, :integer
+
+    Section.all.each do |s|
+      c = ActiveRecord::Base.connection.execute("SELECT * FROM course_semesters WHERE id = #{s.course_semester_id}").to_a
+      s.semester_id = c[2]
+      s.save
+    end
+
   end
 end
