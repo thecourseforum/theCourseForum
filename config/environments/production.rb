43868533cb374f3882c9b3f5dd42305a9f20348e
Justin Dao <tjd5at@virginia.edu>
2014-07-14 22:05:24 -0400

    Updates mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,6 +81,16 @@ TheCourseForum::Application.configure do
 
   #SMTP
   config.action_mailer.default_url_options = {:host => 'thecourseforum.com'}
-  config.action_mailer.delivery_method = :sendmail
+  # config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :enable_starttls_auto => true,
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :domain => "gmail.com",
+      :authentication => :login,
+      :user_name => ENV["EMAIL_USERNAME"],
+      :password => ENV["EMAIL_PASSWORD"]
+  }
   
 end
