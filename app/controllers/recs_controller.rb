cc0fbfb41ab3e543a7a91687cd5089855f935676
DC00 <djc4ku@virginia.edu>
2015-02-28 21:15:08 -0500

    updated recommendation slim

--- app/controllers/recs_controller.rb
+++ app/controllers/recs_controller.rb
@@ -1,7 +1,7 @@
 class RecsController < ApplicationController
 
   def courselist
-    @courses = Course.all(:limit => 10)
+    @courses = Course.all.sample(10)
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @reviews }
