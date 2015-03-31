2b3a3acbbb582c7522d0d8848d8083e4e1ec054e
jt4uc <jt4uc@virginia.edu>
2015-03-30 23:21:05 -0400

    Calculated avg rating and displaying it

--- app/controllers/professors_controller.rb
+++ app/controllers/professors_controller.rb
@@ -16,6 +16,21 @@ class ProfessorsController < ApplicationController
   def show
     @professor = Professor.find(params[:id])
     @courses = @professor.courses_list.sort_by{|c| [c.subdepartment.mnemonic, c.course_number]}
+    @total_reviews = 0
+    @avg_rating = 0;
+
+    @courses.each do |c|
+      @all_reviews = @professor ? Review.where(:course_id => c.id, :professor_id => @professor.id) : Review.where(:course_id => c.id)
+      @total_reviews += @all_reviews.count.to_f
+    end
+
+    @courses.each do |c|
+      @all_reviews = @professor ? Review.where(:course_id => c.id, :professor_id => @professor.id) : Review.where(:course_id => c.id)
+      @avg_rating_hash = get_review_ratings
+      @avg_rating += @avg_rating_hash / @total_reviews
+    end      
+
+    @avg_rating = @avg_rating.round(2)
 
     respond_to do |format|
       format.html # show.html.haml
@@ -29,4 +44,23 @@ class ProfessorsController < ApplicationController
       params.require(:professor).permit(:email_alias, :middle_name, :preferred_name)
     end
 
+    def get_review_ratings
+      ratings = {
+        prof: 0,
+        enjoy: 0,
+        difficulty: 0,
+        recommend: 0
+      }
+
+
+
+      @all_reviews.each do |r|
+        ratings[:prof] += r.professor_rating
+        ratings[:enjoy] += r.enjoyability
+        ratings[:difficulty] += r.difficulty
+        ratings[:recommend] += r.recommend
+      end
+
+      ratings[:overall] = (ratings[:prof] + ratings[:enjoy] + ratings[:recommend]) / 3 
+    end
 end
