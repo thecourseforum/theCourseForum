0466d4ac4b3fd2f1f3cc785e5371a8f97d7a8121
Lawrence Hook <lawrencehook@gmail.com>
2015-06-12 18:51:16 -0400

    Finished SQL commands foor master db. Results in identical schema.

--- db_changes.sql
+++ db_changes.sql
@@ -1,19 +1,27 @@
 # Author: Lawrence Hook
 
-# SQL statements to fix production db,
-# 	so it matches the beta db.
+# SQL changes from master to beta
 
-# Note: run statements after $ rake db:migrate
+# In summary
+#   Added:
+#     book_requirements,
+#     books,
+#     bugs,
+#     calendar_sections,
+#     courses_users,
+#     professor_salary,
+#     schedules_sections
+#   Altered: 
+#     professors
+#   Dropped: 
+#     section_users
 
-# In summary, production db needs the following tables...
-#	  Added: books, book_requirements, professor_salary, schedules_sections
-#	  Modified: professors
-#	  Dropped: section_users
-
-
-# TODO? 
-# Migration of data for: `professor_salary` table, the new columns of `professors` table
-
+CREATE TABLE `book_requirements` (
+  `section_id` int(11) NOT NULL,
+  `book_id` int(11) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  UNIQUE KEY `index_book_requirements_on_book_id_and_section_id` (`book_id`,`section_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `books` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -41,11 +49,29 @@ CREATE TABLE `books` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
-CREATE TABLE `book_requirements` (
-  `section_id` int(11) NOT NULL,
-  `book_id` int(11) NOT NULL,
-  `status` varchar(255) NOT NULL,
-  UNIQUE KEY `index_book_requirements_on_book_id_and_section_id` (`book_id`,`section_id`) USING BTREE
+CREATE TABLE `bugs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `url` varchar(255) DEFAULT NULL,
+  `description` text,
+  `email` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+CREATE TABLE `calendar_sections` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `section_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+CREATE TABLE `courses_users` (
+  `course_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  UNIQUE KEY `index_courses_users_on_user_id_and_course_id` (`user_id`,`course_id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
@@ -59,16 +85,6 @@ CREATE TABLE `professor_salary` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-CREATE TABLE `schedules_sections` (
-  `schedule_id` int(11) NOT NULL,
-  `section_id` int(11) NOT NULL,
-  UNIQUE KEY `index_schedules_sections_on_schedule_id_and_section_id` (`schedule_id`,`section_id`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
-DROP TABLE `sections_users`;
-
 
 ALTER TABLE professors
 ADD COLUMN `classification` text,
@@ -85,21 +101,22 @@ ADD COLUMN `home_page` text,
 ADD COLUMN `mobile_phone` text,
 ADD COLUMN `professor_salary_id` int(11) DEFAULT NULL;
 
-
-ALTER TABLE schedules
-ADD COLUMN `flagged` boolean
-AFTER user_id;
-
-ALTER TABLE bugs
-ADD COLUMN `email` varchar(255) DEFAULT NULL AFTER `description`;
-
-
-
-
+CREATE TABLE `schedules` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `flagged` tinyint(1) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
+CREATE TABLE `schedules_sections` (
+  `schedule_id` int(11) NOT NULL,
+  `section_id` int(11) NOT NULL,
+  UNIQUE KEY `index_schedules_sections_on_schedule_id_and_section_id` (`schedule_id`,`section_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
-
-
-# I'm getting confused with bugs table
+DROP TABLE `section_users`;
