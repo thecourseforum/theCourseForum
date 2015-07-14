432f25ebb015dd78996c3d24458d86e3425d73f4
Alan Wei <awei@rpdway.com>
2015-07-14 14:54:48 -0400

    renamed migration to match file

--- db/migrate/20150714174932_add_last_taught_semester_id_to_courses.rb
+++ db/migrate/20150714174932_add_last_taught_semester_id_to_courses.rb
@@ -1,4 +1,4 @@
-class AddLastSemesterTaughtToCourses < ActiveRecord::Migration
+class AddLastTaughtSemesterIdToCourses < ActiveRecord::Migration
   def change
     add_column :courses, :last_taught_semester_id, :integer
   end
