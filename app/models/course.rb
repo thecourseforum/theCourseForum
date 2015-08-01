b4422731a0972d77544c7cc162d1701b06cc0381
Lawrence Hook <lawrencehook@gmail.com>
2015-08-01 00:39:31 -0400

    fix last taught semester

--- app/models/course.rb
+++ app/models/course.rb
@@ -59,7 +59,8 @@ class Course < ActiveRecord::Base
   def self.update_last_taught_semester
     # Max(semester_id) works because Fall 2009 w/ id 25 (the largest) has 0 sections
     ActiveRecord::Base.connection.execute("SELECT courses.id, max(semesters.number) from courses join sections ON courses.id=sections.course_id join semesters ON semesters.id=sections.semester_id group by courses.id;").each do |pair|
-      Course.find_by(pair.first).update(:last_taught_semester_id => pair.second)
+      Course.find(pair.first).update(:last_taught_semester_id => (Semester.find_by(:number => pair.second).id))
+      # Course.find_by(pair.first).update(:last_taught_semester_id => pair.second)
     end
   end
 
