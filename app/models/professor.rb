60e96dc55337ced030aa77b202f300eaa5a96296
Lawrence Hook <lawrencehook@gmail.com>
2015-08-04 16:22:26 -0400

    Enhance back navigation capabilities in relation to the display course page.

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -19,7 +19,7 @@ class Professor < ActiveRecord::Base
   end
 
   def email
-    if self.email_alias
+    if self.email_alias && self.email_alias.length != 0
       if self.email_alias.include?("@")
         self.email_alias
       else
