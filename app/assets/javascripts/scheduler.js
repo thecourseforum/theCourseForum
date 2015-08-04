d46cdcd885bcc8b89c7aa12aa5822c9cdf1c628c
Lawrence Hook <lawrencehook@gmail.com>
2015-08-04 13:38:03 -0400

    Style autocomplete, fix scheduler search, add class method: Course.current

--- app/assets/javascripts/scheduler.js
+++ app/assets/javascripts/scheduler.js
@@ -211,19 +211,16 @@ $(document).ready(function() {
 	$('#class-search').autocomplete({
 		source: function(request, response) {
 			$.ajax({
-				url: '/search/search_subdepartment',
-				dataType: 'json',
-				type: 'GET',
+				url: '/scheduler/search',
 				data: {
 					query: request.term
 				},
 				success: function(data) {
-					response($.map(data, function(item) {
+					response($.map(data.results, function(item) {
 						return {
-							label: item.mnemonic_number + " " + item.title,
-							value: item.mnemonic_number,
-							course_id: item.course_id
-						}
+							label: item,
+							value: request.term
+						};
 					}));
 				}
 			});
@@ -232,8 +229,9 @@ $(document).ready(function() {
 			$('.ui-autocomplete').css('width', ($('#class-search').parent().width()));
 		},
 		select: function(event, ui) {
-			courseSearch(ui.item.value);
-		}
+			courseSearch(ui.item.label);
+		},
+		minLength: 2
 	});
 
 	// Added search button functionality
