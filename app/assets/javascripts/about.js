9806bf91afc4aee6a9795e4cd88b0ff4c932aeea
Christine Tsou <cyt3ea@virginia.edu>
2015-10-04 00:46:56 -0400

    Image hover

---
+++ app/assets/javascripts/about.js
@@ -0,0 +1,15 @@
+var ready;
+
+ready = function() {
+
+	$('.indivblock').hover(function() {
+	        //$(this).find('.usmain').fadeOut(50);
+	        $(this).find('.ushover').fadeIn();
+	    }, function() {
+	        $(this).find('.ushover').fadeOut();
+	        //$(this).find('.usmain').show();
+	});
+}
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
