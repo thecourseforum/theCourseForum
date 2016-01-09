7fa857de26504cc2bc709672e7d1b79d4c6faa92
cyt3ea <cyt3ea@virginia.edu>
2016-01-09 15:51:59 -0500

    increased orange subtitle padding, increased p line-height, banner adjusts with window size, added alumns

--- app/assets/javascripts/about.js
+++ app/assets/javascripts/about.js
@@ -2,13 +2,10 @@ var ready;
 
 ready = function() {
 
-	$('.block').hover(function() {
-	        //$(this).find('.usmain').fadeOut(50);
-	        $(this).find('.ushover').fadeIn();
-	    }, function() {
-	        $(this).find('.ushover').fadeOut();
-	        //$(this).find('.usmain').show();
-	});
+	$('.block').hover(
+		function() { $(this).find('.ushover').fadeIn(); },
+		function() { $(this).find('.ushover').fadeOut(); }
+		);
 }
 
 $(document).ready(ready);
