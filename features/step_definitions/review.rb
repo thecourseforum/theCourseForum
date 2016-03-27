fe3d20573be5ec1a1c4b0116afbd44eb0e84ee71
Alan Wei <aw3as@virginia.edu>
2016-03-27 17:11:28 -0400

    fix review spec

--- features/step_definitions/review.rb
+++ features/step_definitions/review.rb
@@ -4,7 +4,7 @@ When /^I write a review/ do
   puts "#{Professor.first.last_name}, #{Professor.first.first_name}"
   select "Bloomfield, Aaron", :from => 'prof_select'
   select "Fall", :from => 'semester_season'
-  select "2015", :from => 'semester_year'
+  select "2016", :from => 'semester_year'
   page.execute_script("$('.prof-rating-slider').slider('value', 1)")
   page.execute_script("$('.enjoyability-slider').slider('value', 2)")
   page.execute_script("$('.difficulty-slider').slider('value', 3)")
