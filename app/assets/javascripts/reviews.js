944fbe7817cc4764aa7d1669c3fd6fdb5b6fe9c6
Kienan Adams <kra8ff@virginia.edu>
2016-04-20 21:37:54 -0400

    Remove rating sliders and replace with segmented select

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -88,120 +88,146 @@ ready = function() {
 
 		});
 
-		var prof_radios = $("input[id*=review_professor_rating]");
-		var selected_prof = $("input[id*=review_professor_rating]:checked");
-		if (selected_prof.length > 0) {
-			var prof_val = selected_prof.val();
-			$(".current-prof-rating").text(prof_val);
-		} else {
-			var prof_val = 3;
-			prof_radios[2].click();
-			$(".current-prof-rating").text(prof_val);
-		}
-
-		$(".prof-rating-slider").slider({
-			step: 1,
-			min: 100,
-			max: 500,
-			value: prof_val * 100,
-			slide: function(event, ui) {
-				var val = parseInt((ui.value + 50) / 100);
-				prof_radios[val - 1].click();
-				$(".current-prof-rating").text(val);
-			},
-			stop: function(event, ui) {
-				var val = parseInt((ui.value + 50) / 100) * 100;
-				$(".prof-rating-slider").slider('value', val);
-			}
+		$(function() {
+			$('#professor_rating').barrating({
+				theme: 'bars-movie'
+			});
 		});
 
-		var enjoy_radios = $("input[id*=review_enjoyability]");
-		var selected_enjoy = $("input[id*=review_enjoyability]:checked");
-		if (selected_enjoy.length > 0) {
-
-			var enjoy_val = selected_enjoy.val();
-			$(".current-enjoyability").text(enjoy_val);
-		} else {
-			var enjoy_val = 3;
-			enjoy_radios[2].click();
-			$(".current-enjoyability").text(enjoy_val);
-		}
-
-		$(".enjoyability-slider").slider({
-			step: 1,
-			min: 100,
-			max: 500,
-			value: enjoy_val * 100,
-			slide: function(event, ui) {
-				var val = parseInt((ui.value + 50) / 100);
-				enjoy_radios[val - 1].click();
-				$(".current-enjoyability").text(val);
-			},
-			stop: function(event, ui) {
-				var val = parseInt((ui.value + 50) / 100) * 100;
-				$(".enjoyability-slider").slider('value', val);
-			}
+		$(function() {
+			$('#enjoyability').barrating({
+				theme: 'bars-movie'
+			});
 		});
 
-		var diff_radios = $("input[id*=review_difficulty]");
-		var selected_difficulty = $("input[id*=review_difficulty]:checked");
-		if (selected_difficulty.length > 0) {
-
-			var diff_val = selected_difficulty.val();
-			$(".current-difficulty").text(diff_val);
-		} else {
-			var diff_val = 3;
-			diff_radios[2].click();
-			$(".current-difficulty").text(diff_val);
-		}
-
-		$(".difficulty-slider").slider({
-			step: 1,
-			min: 100,
-			max: 500,
-			value: diff_val * 100,
-			slide: function(event, ui) {
-				var val = parseInt((ui.value + 50) / 100);
-				diff_radios[val - 1].click();
-				$(".current-difficulty").text(val);
-			},
-			stop: function(event, ui) {
-				var val = parseInt((ui.value + 50) / 100) * 100;
-				$(".difficulty-slider").slider('value', val);
-			}
+		$(function() {
+			$('#difficulty').barrating({
+				theme: 'bars-movie'
+			});
 		});
 
-		var recommend_radios = $("input[id*=review_recommend]");
-		var selected_recommend = $("input[id*=review_recommend]:checked");
-		if (selected_recommend.length > 0) {
-
-			var recommend_val = selected_recommend.val();
-			$(".current-recommend").text(recommend_val);
-		} else {
-			var recommend_val = 3;
-			recommend_radios[2].click();
-			$(".current-recommend").text(recommend_val);
-		}
-
-		$(".recommend-slider").slider({
-			step: 1,
-			min: 100,
-			max: 500,
-			value: recommend_val * 100,
-			slide: function(event, ui) {
-				var val = parseInt((ui.value + 50) / 100);
-				recommend_radios[val - 1].click();
-				$(".current-recommend").text(val);
-			},
-			stop: function(event, ui) {
-				var val = parseInt((ui.value + 50) / 100) * 100;
-				$(".recommend-slider").slider('value', val);
-			}
+		$(function() {
+			$('#recommend').barrating({
+				theme: 'bars-movie'
+			});
 		});
+	
+
+
+		// var prof_radios = $("input[id*=review_professor_rating]");
+		// var selected_prof = $("input[id*=review_professor_rating]:checked");
+		// if (selected_prof.length > 0) {
+		// 	var prof_val = selected_prof.val();
+		// 	$(".current-prof-rating").text(prof_val);
+		// } else {
+		// 	var prof_val = 3;
+		// 	prof_radios[2].click();
+		// 	$(".current-prof-rating").text(prof_val);
+		// }
+
+		// $(".prof-rating-slider").slider({
+		// 	step: 1,
+		// 	min: 100,
+		// 	max: 500,
+		// 	value: prof_val * 100,
+		// 	slide: function(event, ui) {
+		// 		var val = parseInt((ui.value + 50) / 100);
+		// 		prof_radios[val - 1].click();
+		// 		$(".current-prof-rating").text(val);
+		// 	},
+		// 	stop: function(event, ui) {
+		// 		var val = parseInt((ui.value + 50) / 100) * 100;
+		// 		$(".prof-rating-slider").slider('value', val);
+		// 	}
+		// });
+
+		// var enjoy_radios = $("input[id*=review_enjoyability]");
+		// var selected_enjoy = $("input[id*=review_enjoyability]:checked");
+		// if (selected_enjoy.length > 0) {
+
+		// 	var enjoy_val = selected_enjoy.val();
+		// 	$(".current-enjoyability").text(enjoy_val);
+		// } else {
+		// 	var enjoy_val = 3;
+		// 	enjoy_radios[2].click();
+		// 	$(".current-enjoyability").text(enjoy_val);
+		// }
+
+		// $(".enjoyability-slider").slider({
+		// 	step: 1,
+		// 	min: 100,
+		// 	max: 500,
+		// 	value: enjoy_val * 100,
+		// 	slide: function(event, ui) {
+		// 		var val = parseInt((ui.value + 50) / 100);
+		// 		enjoy_radios[val - 1].click();
+		// 		$(".current-enjoyability").text(val);
+		// 	},
+		// 	stop: function(event, ui) {
+		// 		var val = parseInt((ui.value + 50) / 100) * 100;
+		// 		$(".enjoyability-slider").slider('value', val);
+		// 	}
+		// });
+
+		// var diff_radios = $("input[id*=review_difficulty]");
+		// var selected_difficulty = $("input[id*=review_difficulty]:checked");
+		// if (selected_difficulty.length > 0) {
+
+		// 	var diff_val = selected_difficulty.val();
+		// 	$(".current-difficulty").text(diff_val);
+		// } else {
+		// 	var diff_val = 3;
+		// 	diff_radios[2].click();
+		// 	$(".current-difficulty").text(diff_val);
+		// }
+
+		// $(".difficulty-slider").slider({
+		// 	step: 1,
+		// 	min: 100,
+		// 	max: 500,
+		// 	value: diff_val * 100,
+		// 	slide: function(event, ui) {
+		// 		var val = parseInt((ui.value + 50) / 100);
+		// 		diff_radios[val - 1].click();
+		// 		$(".current-difficulty").text(val);
+		// 	},
+		// 	stop: function(event, ui) {
+		// 		var val = parseInt((ui.value + 50) / 100) * 100;
+		// 		$(".difficulty-slider").slider('value', val);
+		// 	}
+		// });
+
+		// var recommend_radios = $("input[id*=review_recommend]");
+		// var selected_recommend = $("input[id*=review_recommend]:checked");
+		// if (selected_recommend.length > 0) {
+
+		// 	var recommend_val = selected_recommend.val();
+		// 	$(".current-recommend").text(recommend_val);
+		// } else {
+		// 	var recommend_val = 3;
+		// 	recommend_radios[2].click();
+		// 	$(".current-recommend").text(recommend_val);
+		// }
+
+		// $(".recommend-slider").slider({
+		// 	step: 1,
+		// 	min: 100,
+		// 	max: 500,
+		// 	value: recommend_val * 100,
+		// 	slide: function(event, ui) {
+		// 		var val = parseInt((ui.value + 50) / 100);
+		// 		recommend_radios[val - 1].click();
+		// 		$(".current-recommend").text(val);
+		// 	},
+		// 	stop: function(event, ui) {
+		// 		var val = parseInt((ui.value + 50) / 100) * 100;
+		// 		$(".recommend-slider").slider('value', val);
+		// 	}
+		// });
 	}
 
 
-	
+
 }
 
 $(document).ready(ready);
