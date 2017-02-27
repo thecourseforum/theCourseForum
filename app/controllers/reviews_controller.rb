046e9b651bbbcabf6d49a0911a5aa68331abc795
Christine Tsou <cyt3ea@virginia.edu>
2017-02-26 21:38:43 -0500

    remove @import variables from css and new stat

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -13,9 +13,11 @@ class ReviewsController < ApplicationController
     @reviews_map[nil] = @reviews.where(semester_id: nil).sort_by{|r| r.course.mnemonic_number}
 
     average_prof_rating = 0
+    @review_upvotes = 0
 
     @reviews.each do |r|
       average_prof_rating += r.overall
+      @review_upvotes += r.votes_for
     end
 
     @avg_prof_rating = (average_prof_rating/@reviews.count).round(2)
