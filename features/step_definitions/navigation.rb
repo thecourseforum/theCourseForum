f5292294e07bc7f11a40fafd53bdc4556459f5e8
Christine Tsou <cyt3ea@virginia.edu>
2017-06-26 22:35:17 -0700

    Fix tests 5

--- features/step_definitions/navigation.rb
+++ features/step_definitions/navigation.rb
@@ -7,7 +7,7 @@ When /^I click on the dropdown$/ do
 end
 
 When /^I click the link '([^"]*)'$/ do |text|
-  find('a', :text => text).first.click
+  first('a', :text => text).click
 end
 
 Then /^I should see '([^"]*)'$/ do |text|
