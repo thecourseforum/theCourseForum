ed5be9bdefff495e599bcb0b68956d86063d5f83
Christine Tsou <cyt3ea@virginia.edu>
2017-08-30 14:01:21 -0700

    Fix test 3

--- features/step_definitions/search.rb
+++ features/step_definitions/search.rb
@@ -1,9 +1,7 @@
 When /^I search for '([^"]*)'$/ do |text|
   fill_in('search-query', :with => text)
   sleep 0.2
-  within('.navbar-right') do
-    find('search-query').native.send_key(:enter)
-  end
+  find('#search-query').native.send_key(:enter)
 end
 
 When /^I click on Program and Data Representation$/ do
