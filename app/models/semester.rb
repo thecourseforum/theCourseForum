e0ed6e3ce4d576169051095f8082669a343a68dd
varunpatel111 <vp2xb@virginia.edu>
2018-03-27 21:38:00 -0400

    updating for semester - Fall2018

--- app/models/semester.rb
+++ app/models/semester.rb
@@ -5,7 +5,7 @@ class Semester < ActiveRecord::Base
   has_many :courses, :through => :sections
 
   def self.current
-    Semester.find_by(:year => 2018, :season => 'Spring')
+    Semester.find_by(:year => 2018, :season => 'Fall')
   end
 
   def to_s
--- features/step_definitions/seed.rb
+++ features/step_definitions/seed.rb
@@ -3,8 +3,8 @@ Given /^courses exist$/ do
     school = School.create(:name => 'School of Engineering & Applied Science')
     department = Department.create(:school => school, :name => 'Computer Science')
     subdepartment = Subdepartment.create(:name => 'Computer Science', :mnemonic => 'CS')
-    lastest_semester = Semester.create(:number => 1182, :season => 'Spring', :year => 2018)
-    older_semester = Semester.create(:number => 1178, :season => 'Fall', :year => 2017)
+    lastest_semester = Semester.create(:number => 1188, :season => 'Fall', :year => 2018)
+    older_semester = Semester.create(:number => 1182, :season => 'Spring', :year => 2018)
     department.subdepartments << subdepartment
     course = Course.create(
       :title => 'Program and Data Representation',
