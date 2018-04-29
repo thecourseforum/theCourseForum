266c36dab068d786f8035bb3c39c33e8d2dac868
Brady Madden <bradymadden97@gmail.com>
2018-04-29 20:53:17 +0200

    trying to address ci

--- spec/models/shared_schedule_spec.rb
+++ spec/models/shared_schedule_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe SharedSchedule, :type => :model do
+RSpec.describe Shared_Schedule, :type => :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
