a63faab0b87336f9a8ba5dec4f5e61e27b5fa6b2
Bradley Knaysi <30380036+bradknaysi@users.noreply.github.com>
2018-10-29 21:14:21 -0400

    commented out hidden_fields b/c they are not used anymore

--- features/step_definitions/review.rb
+++ features/step_definitions/review.rb
@@ -11,9 +11,10 @@ When /^I write a review/ do
   page.execute_script("$('.recommend-slider').slider('value', 4)")
   fill_in('review_comment', :with => "Test Review")
   fill_in('readingfield', :with => "1")
-  fill_in('writingfield', :with => "1")
-  fill_in('homeworkfield', :with => "1")
-  fill_in('groupfield', :with => "1")
+  # decided that the 'readingfield' field would be treated like 'totalfield' (removed the others)
+  #fill_in('writingfield', :with => "1")
+  #fill_in('homeworkfield', :with => "1")
+  #fill_in('groupfield', :with => "1")
   click_button("Submit Review")
 end
 
