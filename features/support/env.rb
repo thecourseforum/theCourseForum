5d4cef956af8a2822ca9c0ba1e18b81fbfefbe27
Brian Yu <brian@byu.io>
2019-04-18 20:32:21 -0400

    Selenium

--- features/support/env.rb
+++ features/support/env.rb
@@ -27,7 +27,7 @@ require 'cucumber/rails'
 #
 ActionController::Base.allow_rescue = false
 
-Capybara.default_driver = :selenium_headless
+Capybara.javascript_driver = :selenium
 
 # Remove/comment out the lines below if your app doesn't have a database.
 # For some databases (like MongoDB and CouchDB) you may need to use :truncation instead.
