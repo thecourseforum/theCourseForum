d8e8dcdca5d51c8c97d32d0f9fe782f377f00ac5
Brian Yu <brian@byu.io>
2019-05-01 18:45:46 -0400

    Change ownership

--- passenger/passenger_init.sh
+++ passenger/passenger_init.sh
@@ -1,2 +1,3 @@
 echo "Precompiling..."
-bundle exec rake assets:precompile db:migrate RAILS_ENV=production'
+bundle exec rake assets:precompile db:migrate RAILS_ENV=production
+chown -R app:app /home/app/theCourseForum
