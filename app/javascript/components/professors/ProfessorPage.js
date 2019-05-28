a0df21662787ac82990af6299aa4ec578730a890
Bradley Knaysi <30380036+bradknaysi@users.noreply.github.com>
2019-05-29 01:30:20 +0200

    Improved course details formating

--- app/javascript/components/professors/ProfessorPage.js
+++ app/javascript/components/professors/ProfessorPage.js
@@ -5,14 +5,86 @@ import PropTypes from "prop-types"
 // rails data is passed to react_component as parameters in views/professors/show.html.slim
 class ProfessorPage extends React.Component {
 
+  /* WORK IN PROGRESS
+  get_course_stats = (props) => {
+    const course_stats = props.course.course.stats
+    for (int i = 0; i < course_stats.length; i++) {
+      let stat = course_stats[i]
+      if (stat.professor.full_name == this.props.professor_name) {
+        return stat[0]
+      }
+    }
+    return NULL
+  }
+  */
+
   render() {
 
     const subdepartment_chunks = this.props.course_groups.map((chunk) => {
       const courses = chunk[1].map((course) =>
         <div key={course.course.id} className="row course-panel">
-          <div className="col-xs-4 name-block data-no-turbolink">
+          <div className="col-xs-4 name-block">
             <div className="row course-title">
               <a href={'/courses/' + course.course.id + '?p=' + this.props.professor_id}>{course.course.title}</a>
+            </div>
+          </div>
+
+          <div className="col-xs-8 details-block">
+            <div className="row info-header">
+              <div className="row prof-info-header">
+
+                <div className="col-xs-4 rating-block">
+                  <div className="row course-icon">
+                    <div className="container rating-container">
+                      <div className="row rating-subheader">
+                        <span>RATING</span>
+                      </div>
+                      <div className="row course-rating-row">
+                        <h4>--</h4>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="col-xs-4 rating-block">
+                  <div className="row course-icon">
+                    <div className="container rating-container">
+                      <div className="row rating-subheader">
+                        <span>DIFFICULTY</span>
+                      </div>
+                      <div className="row course-rating-row">
+                        <h4>--</h4>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="col-xs-4 rating-block">
+                  <div className="row course-icon">
+                    <div className="container rating-container">
+                      <div className="row rating-subheader">
+                        <span>GPA</span>
+                      </div>
+                      <div className="row course-rating-row">
+                        <h4>--</h4>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="col-xs-4 rating-block float-right">
+                  <div className="row course-icon">
+                    <div className="container rating-container">
+                      <div className="row rating-subheader">
+                        <span>LAST TAUGHT</span>
+                      </div>
+                      <div className="row course-rating-row">
+                        <h4>--</h4>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
