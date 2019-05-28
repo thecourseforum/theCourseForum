f94e263f8545467afeae31892de00449d0c7afbb
Bradley Knaysi <30380036+bradknaysi@users.noreply.github.com>
2019-05-28 23:38:34 +0200

    courses are now separated by subdepartment and  course links work

--- app/javascript/components/professors/ProfessorPage.js
+++ app/javascript/components/professors/ProfessorPage.js
@@ -7,22 +7,28 @@ class ProfessorPage extends React.Component {
 
   render() {
 
-    // outer map for "subdepartment_chunk" loop
-    // inner map for "course" loop
-
-    const courses = this.props.courses.map((course) =>
-      <div key={course.course.id} className="row course-panel">
-        <div className="col-xs-4 name-block">
-          <div className="row course-title">
-            <p>{course.course.title}</p>
+    const subdepartment_chunks = this.props.course_groups.map((chunk) => {
+      const courses = chunk[1].map((course) =>
+        <div key={course.course.id} className="row course-panel">
+          <div className="col-xs-4 name-block data-no-turbolink">
+            <div className="row course-title">
+              <a href={'/courses/' + course.course.id + '?p=' + this.props.professor_id}>{course.course.title}</a>
+            </div>
           </div>
         </div>
-      </div>
-    );
+      );
+
+      return (
+        <div className="row">
+          <h3 className="subdepartment-name">{chunk[0].name}</h3>
+          {courses}
+        </div>
+      )
+    });
 
     return (
       <React.Fragment>
-        <div className="professors-container">
+        <div className="container professors-container">
           <div className="row professor-header">
             <div className="col-md-4 professor-name">
               <h1>{this.props.professor_name}</h1>
@@ -39,9 +45,8 @@ class ProfessorPage extends React.Component {
 
           <hr></hr>
 
-          <div className="row professor-courses">
-            {courses}
-          </div>
+          {subdepartment_chunks}
+
         </div>
       </React.Fragment>
     );
