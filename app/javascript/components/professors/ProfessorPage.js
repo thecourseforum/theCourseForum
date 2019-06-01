fa352175c0776614b84e5656373d20c51b558999
Bradley Knaysi <30380036+bradknaysi@users.noreply.github.com>
2019-06-01 15:45:14 +0200

    Refactored react_component with functions to build the UI

--- app/javascript/components/professors/ProfessorPage.js
+++ app/javascript/components/professors/ProfessorPage.js
@@ -10,6 +10,28 @@ class ProfessorPage extends React.Component {
   constructor(props) {
     super(props);
     this.handleCourseClick = this.handleCourseClick.bind(this);
+    this.buildProfessorHeader = this.buildProfessorHeader.bind(this);
+    this.buildProfessorSubdepartments = this.buildProfessorSubdepartments.bind(this);
+    this.buildCourseDetailsBlock = this.buildCourseDetailsBlock.bind(this);
+  }
+
+  buildProfessorHeader() {
+    return (
+      <div className="row professor-header">
+        <div className="col-md-4 professor-name">
+          <h1><b>{this.props.professor_name}</b></h1>
+          <h5>{(this.props.professor_email === null) ? "" : this.props.professor_email}</h5>
+        </div>
+        <div className="col-md-4 avg-rating pull-right">
+          <h1>{this.props.avg_rating}</h1>
+          <p> Average Rating </p>
+        </div>
+        <div className="col-md-4 avg-difficulty pull-right">
+          <h1>{this.props.avg_difficulty}</h1>
+          <p> Average Difficulty </p>
+        </div>
+      </div>
+    );
   }
 
   handleCourseClick(course) {
@@ -17,46 +39,32 @@ class ProfessorPage extends React.Component {
     window.location.href = redirectTo;
   }
 
+  buildCourseDetailsBlock(label, value) {
+    return (
+      <div className="col-xs-2">
+        <div className="row rating-subheader">
+          <span>{label}</span>
+        </div>
+        <div className="row course-rating-row">
+          <h4>{(value === null) ? "--" : value.toFixed(2)}</h4>
+        </div>
+      </div>
+    );
+  }
 
-  render() {
-
-    const professor_subdepartments = this.props.course_groups.map((subdepartment) => {
+  buildProfessorSubdepartments() {
+    const subdepartments = this.props.course_groups.map((subdepartment) => {
       const courses = subdepartment[1].map((course) =>
         <div key={course.course.id} className="row course-panel">
           <div className="col-xs-4">
-            <button className="course-name-block" type="button" onClick={this.handleCourseClick.bind(this, course)}>
+            <button className=" btn btn-primary course-name-block" type="button" onClick={this.handleCourseClick.bind(this, course)}>
               {course.course.title} ({course.course.course_number})
             </button>
           </div>
-
           <div className="col-xs-8 course-details-block">
-            <div className="col-xs-2">
-              <div className="row rating-subheader">
-                <span>RATING</span>
-              </div>
-              <div className="row course-rating-row">
-                <h4>{(course.course_stats.rating === null) ? "--" : course.course_stats.rating.toFixed(2)}</h4>
-              </div>
-            </div>
-
-            <div className="col-xs-2">
-              <div className="row rating-subheader">
-                <span>DIFFICULTY</span>
-              </div>
-              <div className="row course-rating-row">
-              <h4>{(course.course_stats.difficulty === null) ? "--" : course.course_stats.difficulty.toFixed(2)}</h4>
-              </div>
-            </div>
-
-            <div className="col-xs-2">
-              <div className="row rating-subheader">
-                <span>GPA</span>
-              </div>
-              <div className="row course-rating-row">
-              <h4>{(course.course_stats.gpa === null) ? "--" : course.course_stats.gpa.toFixed(2)}</h4>
-              </div>
-            </div>
-
+            {this.buildCourseDetailsBlock.call(this, "RATING", course.course_stats.rating)}
+            {this.buildCourseDetailsBlock.call(this, "DIFFICULTY", course.course_stats.difficulty)}
+            {this.buildCourseDetailsBlock.call(this, "GPA", course.course_stats.gpa)}
             <div className="col-xs-2 pull-right">
               <div className="row rating-subheader-semester">
                 <span>LAST TAUGHT</span>
@@ -73,32 +81,28 @@ class ProfessorPage extends React.Component {
         <div className="row">
           <h3 className="subdepartment-name">{subdepartment[0].name}</h3>
           {courses}
-          <br></br>
         </div>
-      )
+      );
     });
+
+    return (
+      <div className="row professor-subdepartments">
+        {subdepartments}
+      </div>
+    );
+  }
+
+
+  render() {
+    const professorHeader = this.buildProfessorHeader.call();
+    const professorSubdepartments = this.buildProfessorSubdepartments.call();
 
     return (
       <React.Fragment>
         <div className="container-fluid professors-container">
-          <div className="row professor-header">
-            <div className="col-md-4 professor-name">
-              <h1><b>{this.props.professor_name}</b></h1>
-            </div>
-            <div className="col-md-4 avg-rating pull-right">
-              <h1>{this.props.avg_rating}</h1>
-              <p> Average Rating </p>
-            </div>
-            <div className="col-md-4 avg-difficulty pull-right">
-              <h1>{this.props.avg_difficulty}</h1>
-              <p> Average Difficulty </p>
-            </div>
-          </div>
-
+          {professorHeader}
           <hr></hr>
-
-          {professor_subdepartments}
-
+          {professorSubdepartments}
         </div>
       </React.Fragment>
     );
