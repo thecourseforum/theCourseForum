e38324a93ed560e5e40c2fde4f33c9d61400a8d4
Brian Yu <brian@byu.io>
2019-11-06 20:18:28 -0500

    Update to Spring 2020

--- features/step_definitions/review.rb
+++ features/step_definitions/review.rb
@@ -3,8 +3,8 @@ When /^I write a review/ do
   select "CS 2150", :from => 'course_select'
   puts "#{Professor.first.last_name}, #{Professor.first.first_name}"
   select "Bloomfield, Aaron", :from => 'prof_select'
-  select "Fall", :from => 'semester_season'
-  select "2019", :from => 'semester_year'
+  select "Spring", :from => 'semester_season'
+  select "2020", :from => 'semester_year'
   page.execute_script("$('.prof-rating-slider').slider('value', 1)")
   page.execute_script("$('.enjoyability-slider').slider('value', 2)")
   page.execute_script("$('.difficulty-slider').slider('value', 3)")
@@ -19,7 +19,7 @@ end
 
 When /^I edit a review/ do
   fill_in('review_comment', :with => 'NEW TEXT')
-  select "2019", :from => 'semester_year'
-  select "Fall", :from => 'semester_season'
+  select "2020", :from => 'semester_year'
+  select "Spring", :from => 'semester_season'
   click_button("Submit Review")
 end
