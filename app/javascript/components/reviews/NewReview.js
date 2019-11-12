6bfa03cdf05e0fd40a1ec0c0c971b02994330f44
Harshita Pathipati <hp4hz@virginia.edu>
2019-11-11 20:11:14 -0500

    creat folders for grades microservice

---
+++ app/javascript/components/reviews/NewReview.js
@@ -0,0 +1,27 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+
+class NewReview extends React.Component{
+    
+    constructor(props) {
+        super(props);
+    }
+render () {
+    return (
+        <React.Fragment>
+        <div className="container" >
+            <div className="row">
+            <p> Hello World </p>
+            </div>
+        </div>
+        </React.Fragment>
+    
+
+        )   
+    }
+
+}
+
+export default NewReview
+
